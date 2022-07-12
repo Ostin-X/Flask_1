1f1883268f9e74a85b9f8f76c9ba273f6d4ae8ce
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-12 15:28:11 +0300

    structure

--- src/flask_report/api/routes_api.py
+++ src/flask_report/api/routes_api.py
@@ -1,39 +1,26 @@
 from flask_restful import Resource, abort, Api
 from flask import request, Response, Blueprint
 import dataclasses
-from flasgger import swag_from
-from dict2xml import dict2xml
 from dicttoxml import dicttoxml
 from src.flask_report.config import pilots
 
 api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(api_bp)
 
+
 class DriversAPI(Resource, Response):
-    # @swag_from('drivers.yml')
     def get(self, driver_id=None):
-        if request.method == 'GET' and request.args.get('format') and request.args.get('format') == 'xml':
-            if driver_id and driver_id.upper() in pilots:
-                xml_result = dicttoxml(dataclasses.asdict(pilots[driver_id.upper()]), attr_type=False)
-                return Response(xml_result, content_type='application/xml')
-            elif driver_id:
-                abort(404, message=f'No such driver {driver_id}')
-            else:
-                xml_result = {}
-                for key, value in pilots.items():
-                    xml_result[key] = dataclasses.asdict(value)
-                xml_result = dicttoxml(xml_result, attr_type=False)
-                return Response(xml_result, content_type='application/xml')
+        format_ = request.args.get('format')
+        if driver_id and driver_id.upper() in pilots:
+            result = dataclasses.asdict(pilots[driver_id.upper()])
+        elif driver_id:
+            abort(404, message=f'No such driver {driver_id}')
         else:
-            if driver_id and driver_id.upper() in pilots:
-                return dataclasses.asdict(pilots[driver_id.upper()])
-            elif driver_id:
-                abort(404, message=f'No such driver {driver_id}')
-            else:
-                result = {}
-                for key, value in pilots.items():
-                    result[key] = dataclasses.asdict(pilots[key])
-                return result
+            result = {}
+            for key, value in pilots.items():
+                result[key] = dataclasses.asdict(pilots[key])
+        return result if format_ != 'xml' else Response(dicttoxml(result), content_type='application/xml')
+
 
     def put(self, driver_id):
         print(request.args)
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,12 +3,12 @@ import flask
 
 
 @pytest.mark.parametrize('test_input, format_res, bytes_res1, bytes_res2', [('/api/v1/drivers', 'application/json',
-                                                                             b'{"RIC": {"abbr": "RIC", "name": "Daniel Ricciardo", "team": "Red Bull Racing"',
+                                                                             b'", "lap_time": "No time"}, "VET": {"abbr": "VET", "name": "Sebastian Vettel"',
                                                                              b'main Grosjean", "team": "Haas F1", "lap_time": "0:01:12.930"}, "HAR": {"abbr'),
                                                                             ('/api/v1/drivers?format=xml',
                                                                              'application/xml',
-                                                                             b'<?xml version="1.0" encoding="UTF-8" ?><root><RIC><abbr>RIC</abbr><name>Dani',
-                                                                             b'2.941</lap_time></GAS><HUL><abbr>HUL</abbr><name>Nico Hulkenberg</name><team')])
+                                                                              b'"><abbr type="str">VET</abbr><name type="str">Sebastian Vettel</name><team t',
+                                                                             b'0</lap_time></SAI><PER type="dict"><abbr type="str">PER</abbr><name type="st')])
 def test_drivers_api_v1_json_xml(client, test_input, format_res, bytes_res1, bytes_res2):
     response = client.get(test_input)
     assert response.status_code == 200
@@ -22,8 +22,8 @@ def test_drivers_api_v1_json_xml(client, test_input, format_res, bytes_res1, byt
                                                                              b'01:12.829"}\n'),
                                                                             ('/api/v1/drivers/BOT?format=xml',
                                                                              'application/xml',
-                                                                             b'<?xml version="1.0" encoding="UTF-8" ?><root><abbr>BOT</abbr><name>Valtteri ',
-                                                                             b'Bottas</name><team>Mercedes F1</team><lap_time>0:01:12.434</lap_time></root>')])
+                                                                             b'<?xml version="1.0" encoding="UTF-8" ?><root><abbr type="str">BOT</abbr><nam',
+                                                                             b'e type="str">Valtteri Bottas</name><team type="str">Mercedes F1</team><lap_t')])
 def test_single_driver_api_v1_json_xml(client, test_input, format_res, bytes_res1, bytes_res2):
     response = client.get(test_input)
     assert response.status_code == 200
