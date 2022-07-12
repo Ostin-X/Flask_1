8bce4c9dbe349dcdeb538a8b92134e32579b3849
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-12 15:33:08 +0300

    structure

--- src/flask_report/api/routes_api.py
+++ src/flask_report/api/routes_api.py
@@ -8,23 +8,18 @@ api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(api_bp)
 
 
-class DriversAPI(Resource, Response):
+class DriversAPI(Resource):
     def get(self, driver_id=None):
         format_ = request.args.get('format')
         if driver_id and driver_id.upper() in pilots:
             result = dataclasses.asdict(pilots[driver_id.upper()])
         elif driver_id:
-            abort(404, message=f'No such driver {driver_id}')
+            abort(404, message=f'No driver {driver_id}')
         else:
             result = {}
             for key, value in pilots.items():
                 result[key] = dataclasses.asdict(pilots[key])
         return result if format_ != 'xml' else Response(dicttoxml(result), content_type='application/xml')
-
-
-    def put(self, driver_id):
-        print(request.args)
-        print(request.form)
 
 
 api.add_resource(DriversAPI, '/drivers/<driver_id>', '/drivers')
