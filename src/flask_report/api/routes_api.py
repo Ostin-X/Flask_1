6722b5d0f967d095a123b3c4d76d4bda198f1ee1
foxstudent102416 <Ll6vGQPd2ROf>
2022-07-13 19:07:33 +0300

    report/drivers 3 drivers api

--- src/flask_report/api/routes_api.py
+++ src/flask_report/api/routes_api.py
@@ -11,13 +11,14 @@ api = Api(api_bp)
 class ReportApi(Resource):
     def get(self, driver_id=None):
         format_ = request.args.get('format', 'json')
+        desc = request.args.get('order', 'asc')
         if driver_id and driver_id.upper() in pilots:
             result = dataclasses.asdict(pilots[driver_id.upper()])
         elif driver_id:
             abort(404, message=f'No driver {driver_id}')
         else:
             result = {}
-            for key, value in sorted(pilots.items(), key=lambda x: x[1].position):
+            for key, value in sorted(pilots.items(), key=lambda x: x[1].position, reverse=desc == 'desc'):
                 result[key] = dataclasses.asdict(value)
         if format == 'xml':
             result = dicttoxml(result)
@@ -27,11 +28,13 @@ class ReportApi(Resource):
 class DriversApi(Resource):
     def get(self, driver_id=None):
         format_ = request.args.get('format', 'json')
+        desc = request.args.get('order', 'acs')
         if driver_id and driver_id.upper() not in pilots:
             abort(404, message=f'No driver {driver_id}')
         else:
             result = get_result_list(driver_id)
-        return result if format_ != 'xml' else Response(dicttoxml(result), content_type='application/xml')
+        return result if format_ != 'xml' else Response(dicttoxml(result), content_type='application/xml',
+                                                        reverse=desc == 'desc')
 
 
 def get_result_list(driver_id):
