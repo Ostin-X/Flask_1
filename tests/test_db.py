4c4dd8f99cce1838cc1e76847402c978da8cc3c4
foxstudent102416 <Ll6vGQPd2ROf>
2022-07-18 22:23:19 +0300

    correction 2 SqliteDatabase(None)

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,15 +1,14 @@
 import pytest
-import os
 from src.flask_report.db.models import *
 from src.flask_report.config import *
 
 
 def test_db():
-    # os.remove('pilots.db')
-    # db.create_tables([Team, Pilot, SessionTime])
-    # create_teams(teams)
-    # create_pilots(pilots_list, pilot_nations)
-    # create_times(pilots_list)
+    db.init(':memory:')
+    db.create_tables([Team, Pilot, SessionTime])
+    create_teams(teams)
+    create_pilots(pilots_list, pilot_nations)
+    create_times(pilots_list)
     assert isinstance(db, SqliteDatabase)
     assert Team.get(Team.abbr == 'MER').name == 'Mercedes'
     assert Pilot.get(Pilot.nation == 'United Kingdom').name == 'Lewis Hamilton'
