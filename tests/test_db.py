07870cda3b228bacdd9840a0b30f9efec2c1b263
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-18 13:02:06 +0300

    driver_id check

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,16 +4,13 @@ from src.flask_report.DB.models import *
 from src.flask_report.config import *
 
 
-def test_db1():
-    assert isinstance(db, SqliteDatabase)
-
-
-def test_db2():
+def test_db():
     # os.remove('pilots.db')
     # db.create_tables([Team, Pilot, SessionTime])
     # create_teams(teams)
     # create_pilots(pilots_list, pilot_nations)
     # create_times(pilots_list)
+    assert isinstance(db, SqliteDatabase)
     assert Team.get(Team.abbr == 'MER').name == 'Mercedes'
     assert Pilot.get(Pilot.nation == 'United Kingdom').name == 'Lewis Hamilton'
     assert SessionTime.get(SessionTime.pilot_abbr == 'HAM').lap_time == 'No time'
