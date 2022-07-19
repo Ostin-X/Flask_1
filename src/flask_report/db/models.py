542d93c0bd90e3bb503eb9f66e62d21e55fd0c23
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-19 11:29:22 +0300

    correction 2 SqliteDatabase(None)

--- src/flask_report/db/models.py
+++ src/flask_report/db/models.py
@@ -1,45 +1,47 @@
 from peewee import *
 
-db = SqliteDatabase('pilots.db')
+db = SqliteDatabase(None)
 
 
 class BaseModel(Model):
-    abbr = CharField(primary_key=True)
-    name = CharField()
-    nation = CharField()
-
     class Meta:
         database = db
 
 
 class Team(BaseModel):
+    abbr = CharField()
+    name = CharField()
     engine = CharField()
+    nation = CharField()
     base = CharField()
 
 
 class Pilot(BaseModel):
-    team = ForeignKeyField(Team, backref='pilot', )
+    abbr = CharField()
+    name = CharField()
+    nation = CharField()
+    team = ForeignKeyField(Team, backref='pilot', field='name')
 
 
-class SessionTime(Model):
-    pilot_abbr = ForeignKeyField(Pilot, backref='lap_time')
+class SessionTime(BaseModel):
+    pilot_abbr = ForeignKeyField(Pilot, backref='lap_time', field='abbr')
     lap_time = CharField()
-
-    class Meta:
-        database = db
 
 
 def create_teams(teams):
-    Team.insert_many(teams, fields=[Team.abbr, Team.name, Team.engine, Team.nation, Team.base]).execute()
+    # Team.insert_many(teams, fields=[Team.abbr, Team.name, Team.engine, Team.nation, Team.base]).execute()
+    for team in teams:
+        if not Team.select().where(Team.abbr == team[0]):
+            Team.create(abbr=team[0], name=team[1], engine=team[2], nation=team[3], base=team[4])
 
 
 def create_pilots(pilots, pilot_nations):
     for ab, pi in pilots.items():
-        if not Pilot.select().where(Pilot.abbr == ab).exists():
-            Pilot.create(abbr=ab, name=pi.name, team=pi.team[:3].upper(), nation=pilot_nations[ab])
+        if not Pilot.select().where(Pilot.abbr == ab):
+            Pilot.create(abbr=ab, name=pi.name, team=pi.team, nation=pilot_nations[ab])
 
 
 def create_times(pilots):
     for ab, pi in pilots.items():
-        if not SessionTime.select().where(SessionTime.pilot_abbr == ab).exists():
+        if not SessionTime.select().where(SessionTime.pilot_abbr == ab):
             SessionTime.create(pilot_abbr=ab, lap_time=pi.lap_time)
