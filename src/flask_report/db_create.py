076e4fa7eddf8426b643cbcec0954028a468e59c
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-19 11:38:50 +0300

    correction 2 SqliteDatabase(None)

--- src/flask_report/db_create.py
+++ src/flask_report/db_create.py
@@ -1,6 +1,8 @@
 from src.flask_report.db.models import *
 from config import teams, pilots_list, pilot_nations
 
+db.init('pilots.db')
+
 db.create_tables([Team, Pilot, SessionTime])
 create_teams(teams)
 create_pilots(pilots_list, pilot_nations)
