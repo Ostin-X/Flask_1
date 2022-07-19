18dc28ad200ebb1c17eba5319f7a8d6ca6534595
Rain-X81 <andrewpeshekhonov@gmail.com>
2022-07-19 12:18:52 +0300

    correction 3 no funcs

--- src/flask_report/api/routes_api.py
+++ src/flask_report/api/routes_api.py
@@ -8,11 +8,17 @@ api = Api(api_bp)
 
 
 class ReportApi(Resource):
-    def get(self, driver_id=''):
+    def get(self):
         format_ = request.args.get('format', 'json')
         desc = request.args.get('order', 'asc')
-        result_ = get_result_list(driver_id, desc, 'position')
-        result = get_result_format(result_, format_)
+        result = {}
+        if desc == 'desc':
+            sorted_db = Pilot.select().join(SessionTime).group_by(Pilot).order_by(-SessionTime.lap_time)
+        else:
+            sorted_db = Pilot.select().join(SessionTime).group_by(Pilot).order_by(SessionTime.lap_time)
+        for pilot in sorted_db:
+            result[pilot.abbr] = get_result_pilot(pilot, True)
+        result = get_result_format(result, format_)
         return Response(result, content_type=f'application/{format_}')
 
 
@@ -20,35 +26,26 @@ class DriversApi(Resource):
     def get(self, driver_id=''):
         format_ = request.args.get('format', 'json')
         desc = request.args.get('order', 'acs')
-        result_ = get_result_list(driver_id, desc, 'name')
-        result = get_result_format(result_, format_)
+        selected_db_pilot = Pilot.select().where(Pilot.abbr == driver_id.upper())
+        if not driver_id:
+            result = {}
+            if desc == 'desc':
+                sorted_db = Pilot.select().order_by(-Pilot.abbr)
+            else:
+                sorted_db = Pilot.select().order_by(Pilot.abbr)
+            for pilot in sorted_db:
+                result[pilot.abbr] = get_result_pilot(pilot)
+        elif selected_db_pilot:
+            result = get_result_pilot(selected_db_pilot.get())
+        else:
+            abort(404, message=f'No driver {driver_id}')
+        result = get_result_format(result, format_)
         return Response(result, content_type=f'application/{format_}')
 
 
-def get_result_list(driver_id, desc, sort_param):
-    selected_db_pilot = Pilot.select().where(Pilot.abbr == driver_id.upper())
-    if driver_id and not selected_db_pilot:
-        abort(404, message=f'No driver {driver_id}')
-    elif driver_id:
-        result = get_result_pilot(selected_db_pilot.get(), sort_param)
-    else:
-        result = {}
-        if desc == 'desc' and sort_param == 'name':
-            sorted_db = Pilot.select().order_by(-Pilot.abbr)
-        elif sort_param == 'name':
-            sorted_db = Pilot.select().order_by(Pilot.abbr)
-        elif desc == 'desc' and sort_param == 'position':
-            sorted_db = Pilot.select().join(SessionTime).group_by(Pilot).order_by(-SessionTime.lap_time)
-        else:
-            sorted_db = Pilot.select().join(SessionTime).group_by(Pilot).order_by(SessionTime.lap_time)
-        for pilot in sorted_db:
-            result[pilot.abbr] = get_result_pilot(pilot, sort_param)
-    return result
-
-
-def get_result_pilot(pilot, sort_param):
+def get_result_pilot(pilot, need_lap_time=None):
     result = {'abbr': pilot.abbr, 'name': pilot.name, 'team': pilot.team.name}
-    if sort_param == 'position':
+    if need_lap_time:
         result['lap_time'] = SessionTime.get(SessionTime.pilot_abbr == result['abbr']).lap_time
     return result
 
